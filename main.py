e7b9c818c496abae5307513e64f78718e3022cc5
mayganbook <bookm@students.dsbn.org>
2025-03-24 12:18:08 -0400

    finished

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# Maygan Book
+# March 24/2025
+# Lesson 16
+# Working with lists 
+
+import random 
+numlist=[]
+for i in range(100):
+    num=random.randint(0,100)
+    numlist.append(num)
+print(numlist)
+avg=sum(numlist)/100 
+print(" your average is:" , avg)
 
